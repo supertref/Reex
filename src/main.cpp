0189c4d10c2c7211eb994c3077d89e4abe268b0f
ZioFabry <ziofabry@hotmail.com>
2019-06-09 04:54:12 +0200

    New Version Enforcing implemented

--- src/main.cpp
+++ src/main.cpp
@@ -4630,8 +4630,11 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 
         // REEX: We use certain sporks during IBD, so check to see if they are
         // available. If not, ask the first peer connected for them.
-        bool fMissingSporks = !pSporkDB->SporkExists(SPORK_14_NEW_PROTOCOL_ENFORCEMENT) &&
-                !pSporkDB->SporkExists(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2);
+        bool fMissingSporks = !pSporkDB->SporkExists(SPORK_14_PROTOCOL_ENFORCEMENT_1)
+                           && !pSporkDB->SporkExists(SPORK_15_PROTOCOL_ENFORCEMENT_2)
+                           && !pSporkDB->SporkExists(SPORK_17_PROTOCOL_ENFORCEMENT_3)
+                           && !pSporkDB->SporkExists(SPORK_18_PROTOCOL_ENFORCEMENT_4)
+                           && !pSporkDB->SporkExists(SPORK_19_PROTOCOL_ENFORCEMENT_5);
 
         if (fMissingSporks || !fRequestedSporksIDB){
             LogPrintf("asking peer for sporks\n");
@@ -5344,13 +5347,14 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
     }
 
     else if (strCommand == "reject") {
-        if (fDebug) {
-            try {
-                string strMsg;
-                unsigned char ccode;
-                string strReason;
-                vRecv >> LIMITED_STRING(strMsg, CMessageHeader::COMMAND_SIZE) >> ccode >> LIMITED_STRING(strReason, MAX_REJECT_MESSAGE_LENGTH);
+        string strMsg;
+        unsigned char ccode;
+        string strReason;
 
+        try {
+            vRecv >> LIMITED_STRING(strMsg, CMessageHeader::COMMAND_SIZE) >> ccode >> LIMITED_STRING(strReason, MAX_REJECT_MESSAGE_LENGTH);
+
+            if (fDebug) {
                 ostringstream ss;
                 ss << strMsg << " code " << itostr(ccode) << ": " << strReason;
 
@@ -5360,9 +5364,22 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                     ss << ": hash " << hash.ToString();
                 }
                 LogPrint("net", "Reject %s\n", SanitizeString(ss.str()));
-            } catch (std::ios_base::failure& e) {
-                // Avoid feedback loops by preventing reject messages from triggering a new reject message.
-                LogPrint("net", "Unparseable reject message received\n");
+            }
+        } catch (std::ios_base::failure& e) {
+            // Avoid feedback loops by preventing reject messages from triggering a new reject message.
+            LogPrint("net", "Unparseable reject message received\n");
+        }
+
+        // If I receive a REJECT_OBSOLETE reason I check the current Protocol Version
+        if( ccode == REJECT_OBSOLETE )
+        {
+            if( PROTOCOL_VERSION < ActiveProtocol() )
+            {
+                // My node is too old
+                LogPrintf("Your node is too old (%d), you MUST upgrade to protocol version %d minimum, exiting...", PROTOCOL_VERSION, ActiveProtocol());
+                StartShutdown();
+            } else {
+                // If my node is not too old probably my sporks are not updated
             }
         }
     } else {
@@ -5384,20 +5401,22 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 //       it was the one which was commented out
 int ActiveProtocol()
 {
+    if (IsSporkActive(SPORK_19_PROTOCOL_ENFORCEMENT_5))
+        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_5;
 
-    // SPORK_14 was used for 70910. Leave it 'ON' so they don't see > 70910 nodes. They won't react to SPORK_15
-    // messages because it's not in their code
+    if (IsSporkActive(SPORK_18_PROTOCOL_ENFORCEMENT_4))
+        return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT_5;
 
-/*    if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
-            return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
-*/
+    if (IsSporkActive(SPORK_17_PROTOCOL_ENFORCEMENT_3))
+        return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT_4;
 
-    // SPORK_15 is used for 70911. Nodes < 70911 don't see it and still get their protocol version via SPORK_14 and their
-    // own ModifierUpgradeBlock()
+    if (IsSporkActive(SPORK_15_PROTOCOL_ENFORCEMENT_2))
+        return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT_3;
 
-    if (IsSporkActive(SPORK_15_NEW_PROTOCOL_ENFORCEMENT_2))
-            return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
-    return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
+    if (IsSporkActive(SPORK_14_PROTOCOL_ENFORCEMENT_1))
+        return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT_2;
+
+    return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT_1;
 }
 
 // requires LOCK(cs_vRecvMsg)
