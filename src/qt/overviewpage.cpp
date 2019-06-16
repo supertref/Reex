4449cc3dca1f368a03ce569b3c332ed6eca3ca4a
ZioFabry <ziofabry@hotmail.com>
2019-06-16 22:26:04 +0200

    News link updated

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -32,7 +32,7 @@
 #define ICON_OFFSET 16
 #define NUM_ITEMS 7
 
-#define NEWS_URL "https://reecore.org/category/news"
+#define NEWS_URL "https://reecore.org/category/news/feed"
 
 extern CWallet* pwalletMain;
 
